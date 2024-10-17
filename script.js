48b8e52a1d0b84ed020c6dcf19f182dc286e8d4c
Gokul Krishna <119227458+gokulx2@users.noreply.github.com>
2024-10-18 02:58:37 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var str = document.getElementsByTagName('div')[0].innerHTML.toString();
 var i = 0;
-document.getElementsByTagName('div')[0].innerHTML = "";
 
-setTimeout(function() {
-    var se = setInterval(function() {
-        i++;
-        document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
-        if (i == str.length) {
-            clearInterval(se);
-            document.getElementsByTagName('div')[0].innerHTML = str;
-        }
-    }, 10);
-},0);
+function typeWriter() {
+  if (i < str.length) {
+    document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
+    i++;
+    setTimeout(typeWriter, 10);
+  } else {
+    document.getElementsByTagName('div')[0].innerHTML = str;
+  }
+}
+
+typeWriter();
